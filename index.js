7c3f6d450d5c23a50dec57a5b55a3bcfffeb7c7c
Preetheka <80209071+Preetheka@users.noreply.github.com>
2022-02-01 11:17:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
  var randomDiceImage = "dice" + randomNumber1 + ".png";   //dice1.png - dice6.png
 
- var randomImageSource = "images/" + randomDiceImage;  //images/dice1.png - images/dice6.png
+ var randomImageSource =  randomDiceImage;  //images/dice1.png - images/dice6.png
 
  var image1 = document.querySelectorAll("img")[0];
  image1.setAttribute("src", randomImageSource);
@@ -11,7 +11,7 @@
 
  var randomNumber2= Math.floor(Math.random()*6)+1;
 
- var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+ var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
